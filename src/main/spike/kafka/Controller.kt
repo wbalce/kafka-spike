45f57a0e28f92be8901d17cbc644a12c1e9b79bc
Wilkinson Balce <wilkinson.balce@gmail.com>
2022-02-13 12:53:35 +1100

    add callback to report success or failure of sending a message, and refactor publishing to its own component

--- src/main/spike/kafka/Controller.kt
+++ src/main/spike/kafka/Controller.kt
@@ -3,15 +3,13 @@ package spike.kafka
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.kafka.core.KafkaTemplate
 
 @RestController
-class Controller(private val kafkaTemplate: KafkaTemplate<String, String>) {
-    val topicName = "my-topic-name"
+class Controller(private val service: Service) {
 
     @PostMapping("publish/{message}")
     fun getAction(@PathVariable message: String) {
-        println("Publishing message '$message' to topic '$topicName'")
-        kafkaTemplate.send(topicName, message)
+        service.sendMessage(message)
     }
 }
+
---
+++ src/main/spike/kafka/Service.kt
@@ -0,0 +1,36 @@
+package spike.kafka
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.util.concurrent.ListenableFutureCallback
+import org.springframework.util.concurrent.ListenableFuture
+import org.springframework.kafka.support.SendResult
+import org.springframework.stereotype.Component
+
+@Component
+open class Service(private val kafkaTemplate: KafkaTemplate<String, String>) {
+    val topicName = "my-topic-name"
+
+    open fun sendMessage(message: String) {
+        println("Publishing message '$message' to topic '$topicName'")
+
+        val future = kafkaTemplate.send(topicName, message)
+
+        future.addCallback(object: ListenableFutureCallback<SendResult<String, String>> {
+            override fun onSuccess(sendResult: SendResult<String, String>) {
+                val message = sendResult.getProducerRecord().value()
+                println("Successfully published message '$message' to topic '$topicName'")
+            }
+
+            override fun onFailure(e: Throwable) {
+                println(
+                    """
+                        ============================================================
+                        Unsuccessfully published message '$message' to topic '$topicName'
+                        ============================================================
+                    """.trimIndent()
+                )
+            }
+        })
+    }
+}
+
